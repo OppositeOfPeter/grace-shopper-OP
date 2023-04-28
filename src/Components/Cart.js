a034dc99a6df47f04e96c9f6e2f3e1875c01e047
Marie Tornetto <marietornetto@gmail.com>
2023-04-28 00:52:52 +0000

    commit

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,19 +1,59 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../store';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCart, logout, deleteItem, createItem, createOrder } from "../store";
+import { Link, useNavigate } from "react-router-dom";
 
-const Cart = ()=> {
-  const { cart } = useSelector(state => state);
+const Cart = () => {
+  const { cart } = useSelector((state) => state);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const lineItems = cart.lineItems;
+
+  const deleteLineItem = async (lineItem) => {
+    const product = lineItem.product;
+    dispatch(deleteItem({ product, quantityToRemove: 1 }));
+    navigate("/cart");
+  };
+
+  const createLineItem = async (lineItem) => {
+    const product = lineItem.product;
+    await dispatch(createItem({ product, quantity: 1 }));
+    navigate("/cart");
+  };
+  
+  const createOrderFromCart = async() => {
+    await dispatch(createOrder());
+    navigate("/cart");
+  };
+
   return (
     <div>
       <h1>Cart</h1>
-      <pre>
-        {
-          JSON.stringify(cart, null, 2)
-        }
-      </pre>
+      {/* I think we can remove this line below */}
+      {/* <pre>{JSON.stringify(cart, null, 2)}</pre> */}
+      <ul>
+        {lineItems.map((lineItem) => {
+          return lineItem ? (
+            <li key={lineItem.id}>
+              {lineItem.product.title} - {lineItem.quantity}
+              <button onClick={() => deleteLineItem(lineItem)}>
+                remove 1 from cart
+              </button>
+              <button onClick={() => createLineItem(lineItem)}>
+                add 1 to cart
+              </button>
+            </li>
+          ) : (
+            ""
+          );
+        })}
+      </ul>
+      <div>
+        <button onClick={() => createOrderFromCart(cart)}>
+          Create Order
+        </button>
+      </div>
     </div>
   );
 };
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,21 +1,66 @@
-import axios from 'axios';
-const cart = (state = { lineItems: [] }, action)=> {
-  if(action.type === 'SET_CART'){
+import axios from "axios";
+const cart = (state = { lineItems: [] }, action) => {
+  if (action.type === "SET_CART") {
+    return action.cart;
+  }
+  if (action.type === "CREATE_ITEM") {
+    state = { lineItems: [...state.lineItems, action.lineItems] };
+    return action.cart ? action.cart : null;
+  }
+  if (action.type === "DELETE_ITEM") {
+    return action.cart;
+  }
+  if (action.type === "SET_ORDER") {
     return action.cart;
   }
   return state;
 };
 
-
-export const fetchCart = ()=> {
-  return async(dispatch)=> {
-    const token = window.localStorage.getItem('token');
-    const response = await axios.get('/api/orders/cart', {
+export const fetchCart = () => {
+  return async (dispatch) => {
+    const token = window.localStorage.getItem("token");
+    const response = await axios.get("/api/orders/cart", {
       headers: {
-        authorization: token
-      }
+        authorization: token,
+      },
     });
-    dispatch({ type: 'SET_CART', cart: response.data });
+    dispatch({ type: "SET_CART", cart: response.data });
+  };
+};
+
+export const createItem = (cart) => {
+  const token = window.localStorage.getItem("token");
+  return async (dispatch) => {
+    const response = await axios.post("/api/orders/cart", cart, {
+      headers: {
+        authorization: token,
+      },
+    });
+    dispatch({ type: "CREATE_ITEM", cart: response.data });
+  };
+};
+
+export const deleteItem = (cart) => {
+  return async (dispatch) => {
+    const token = window.localStorage.getItem("token");
+    const response = await axios.put("/api/orders/cart", cart, {
+      headers: {
+        authorization: token,
+      },
+    });
+    dispatch({ type: "DELETE_ITEM", cart: response.data });
+  };
+};
+
+export const createOrder = () => {
+  return async (dispatch) => {
+    const token = window.localStorage.getItem("token");
+    const response = await axios.post("/api/orders", {
+      headers: {
+        authorization: token,
+      },
+    });
+    dispatch({ type: "SET_ORDER", order: response.data });
   };
 };
 
