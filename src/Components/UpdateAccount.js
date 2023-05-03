a10cda68239504b3cfcd3f29e0bd21af38154370
Angel Gao <angelgao9@gmail.com>
2023-05-02 23:53:17 -0400

    added address model

--- src/Components/UpdateAccount.js
+++ src/Components/UpdateAccount.js
@@ -1,42 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { updateAuth } from '../store';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateAuth } from "../store";
+import { useNavigate } from "react-router-dom";
 
 const UpdateAccount = () => {
-    
-  const { auth } = useSelector(state => state);
+  const { auth } = useSelector((state) => state);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  
-  const updateUser = async(ev) => {
+  const updateUser = async (ev) => {
     ev.preventDefault();
     await dispatch(updateAuth({ username, password }));
-    setUsername('');
-    setPassword('');
-    navigate('/profile');
-  };  
-    
-  
+    setUsername("");
+    setPassword("");
+    navigate("/profile");
+  };
+
+  const addShippingAddress = async (ev) => {
+    navigate("/profile");
+  };
+
   return (
     <div>
       <h2>Update Account</h2>
-      <form onSubmit={ updateUser }>
+      <form onSubmit={updateUser}>
         <label>Username:</label>
-          <input value={ username } onChange={ ev => setUsername(ev.target.value)} />
+        <input
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+        />
         <label>Password:</label>
-          <input value={ password } onChange={ ev => setPassword(ev.target.value)} />
+        <input
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
         <button>Update Account</button>
       </form>
+      <h2>Add Shipping Address</h2>
+      <form onSubmit={addShippingAddress}>
+        <label></label>
+        <input></input>
+        <label></label>
+        <input></input>
+        <label></label>
+        <input></input>
+        <label></label>
+        <input></input>
+        <button>Add Shipping Address</button>
+      </form>
     </div>
-  );  
-    
+  );
 };
 
-
 export default UpdateAccount;
-
