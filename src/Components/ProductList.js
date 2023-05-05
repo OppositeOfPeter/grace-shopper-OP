442de73dee855c34f46739626adc9a1ac7015a9a
Angel Gao <angelgao9@gmail.com>
2023-05-04 20:27:38 -0400

    fixed error

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,33 +1,31 @@
 // src/components/Products.js
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchProducts } from "../store";
+import { fetchProducts, fetchReviews } from "../store";
 import { Link, useNavigate } from "react-router-dom";
 import { addToCart } from "../store/cart";
 import SingleProduct from "./SingleProduct";
 import SearchBar from "./SearchBar";
 
-
 const Products = ({}) => {
-	const [product, setProduct] = useState(null);
-	const { products, reviews } = useSelector((state) => state);
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+  const [product, setProduct] = useState(null);
+  const { products, reviews } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	const [filteredProducts, setFilteredProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-	const handleClick = (productId) => {
-		setProduct(productId);
-	};
+  const handleClick = (productId) => {
+    setProduct(productId);
+  };
 
-	// Fetch the list of products when the component mounts.
-	useEffect(() => {
-		dispatch(fetchProducts());
-		dispatch(fetchReviews());
-	}, []);
+  // Fetch the list of products when the component mounts.
+  useEffect(() => {
+    dispatch(fetchProducts());
+    dispatch(fetchReviews());
+  }, []);
 
   const createLineItem = async (product) => {
-
     //await dispatch(createItem({ product, quantity: 1 }));
 
     await dispatch(addToCart(product));
@@ -35,43 +33,43 @@ const Products = ({}) => {
     navigate("/cart");
   };
 
-	//for the searchbar
-	const handleSearch = (searchTerm) => {
-		const filtered = products.filter((product) =>
-			product.title.toLowerCase().includes(searchTerm.toLowerCase())
-		);
-		setFilteredProducts(filtered);
-	};
+  //for the searchbar
+  const handleSearch = (searchTerm) => {
+    const filtered = products.filter((product) =>
+      product.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
 
-	return (
-		<div>
-			<div>
-				<h2>Book Search</h2>
-				<p>Search by Title</p>
-				<SearchBar
-					data={products.map((product) => product.title)}
-					onSearch={handleSearch}
-				/>
-				<div>
-					{filteredProducts.map((product) => (
-						<div key={product.id}>
-							<Link to={`/products/${product.id}`}>{product.title}</Link>
-						</div>
-					))}
-				</div>
-			</div>
-			<div>
-				<ul>
-					{products.map((product) => {
-						if (!product) return null;
-						return (
-							<li key={product.id}>
-								<Link to={`/products/${product.id}`} onClick={handleClick}>
-									<img src={product.imageUrl} id="cover-image" />
-								</Link>
-								<Link to={`/products/${product.id}`} onClick={handleClick}>
-									<p>
-										Click for Rating
+  return (
+    <div>
+      <div>
+        <h2>Book Search</h2>
+        <p>Search by Title</p>
+        <SearchBar
+          data={products.map((product) => product.title)}
+          onSearch={handleSearch}
+        />
+        <div>
+          {filteredProducts.map((product) => (
+            <div key={product.id}>
+              <Link to={`/products/${product.id}`}>{product.title}</Link>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <ul>
+          {products.map((product) => {
+            if (!product) return null;
+            return (
+              <li key={product.id}>
+                <Link to={`/products/${product.id}`} onClick={handleClick}>
+                  <img src={product.imageUrl} id="cover-image" />
+                </Link>
+                <Link to={`/products/${product.id}`} onClick={handleClick}>
+                  <p>
+                    Click for Rating
                     {/* {reviews.filter((review) => {
                       review.productId === product.id
                     }).map((review) => {
@@ -84,28 +82,29 @@ const Products = ({}) => {
 										}, 0) / reviews.filter((review) => {
                       review.productId === product.id
                     }).length}{' '} */}
-										
-										{/* {reviews.filter((review) => {
+                    {/* {reviews.filter((review) => {
                       review.productId === product.id
                     }).length}{' '} reviews */}
-									</p>
-								</Link>
-								<Link to={`/products/${product.id}`} onClick={handleClick}>
-									<h4>{product.title}</h4>
-								</Link>
-								<p>by: {product.author}</p>
+                  </p>
+                </Link>
+                <Link to={`/products/${product.id}`} onClick={handleClick}>
+                  <h4>{product.title}</h4>
+                </Link>
+                <p>by: {product.author}</p>
                 <p>${product.price}</p>
-								<button onClick={() => createLineItem(product)}>Add to Cart</button>
-                <br/>
-                <br/>
-							</li>
-						);
-					})}
-				</ul>
-				{product && <SingleProduct productId={product} />}
-			</div>
-		</div>
-	);
+                <button onClick={() => createLineItem(product)}>
+                  Add to Cart
+                </button>
+                <br />
+                <br />
+              </li>
+            );
+          })}
+        </ul>
+        {product && <SingleProduct productId={product} />}
+      </div>
+    </div>
+  );
 };
 
 export default Products;
